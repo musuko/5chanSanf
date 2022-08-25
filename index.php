5637cb32ba8c604c46daf933e6246315d0730af8
musuko <100979661+musuko@users.noreply.github.com>
2022-08-25 10:31:38 +0900

    Add files via upload
    index.htmlを開く
    読みたいタイトルを選択し、名前を付けて保存でsanf.htmlとする。

---
+++ index.php
@@ -0,0 +1,112 @@
+<?php
+require "sanf_select.php";
+// 読み込んだスレッドの一覧を表示する。
+// 不快なスレッドのIDを非表示にする。
+$name = filter_input(INPUT_GET, "name");
+$num = filter_input(INPUT_GET, "num");
+$num = isset($num) ? $num: file_get_contents('last.txt');
+$button = filter_input(INPUT_GET, "button");
+$name = isset($name) ? $name . "\n" : "";
+
+if (isset($num)) {
+    if ($button === "del") {
+        echo $num . "を削除しました";
+
+    } elseif ($button === "over") {
+        file_put_contents("last.txt", $num);
+        echo $num.'まで読んだ';echo '<br>';
+        $name = "";
+    }
+}
+echo "<br> \n";
+//削除した行、読了の行($num)のひとつ前にジャンプ。削除した行にジャンプできないのでひとつ前にする。
+//$numが1より大きい場合。先頭行に表示。
+if ($num > 1) {
+    echo '<a href="#' . ($num - 1) . '">ID = #' . ($num - 1) . 'へジャンプ</a>';
+    $num_mem = $num;
+}
+
+echo "<br> \n";
+file_put_contents("./del.txt", $name, FILE_APPEND);
+
+$file = fopen("./sanf.html", "r");
+$i = 0;
+while ($i <= 7) { //!feof($file)と同じ（falseの間続けますよ）の意味
+    // while (feof($file) === false) { //!feof($file)と同じ（falseの間続けますよ）の意味
+    $line = trim(fgets($file));
+    if ($i === 7) {
+        $line = mb_convert_encoding($line, "utf-8", "sjis"); // シフトJISからUTF-8に変換
+        // var_dump($line);
+
+
+        // こちらはHTMLのまま
+        $line = mb_strstr($line, '★ULA版★', false);   // 後の部分の文字列を抜き出す
+        $line = mb_strstr($line, '/ul>', false);   // 後の部分の文字列を抜き出す
+        $line = mb_strstr($line, 'lass="thread">', false);   // 後の部分の文字列を抜き出す
+        $line = mb_substr($line, 14);
+        // var_dump ($line);
+        $line = mb_strstr($line, '前100</a>', true);   // 前の部分の文字列を抜き出す
+        // var_dump (htmlspecialchars($line, ENT_QUOTES, 'utf-8'));
+        //trueを指定した場合、指定した文字列より前の文字列を取得します。指定しない場合（false）、指定した文字列以降の文字列を取得します。
+
+    }
+    $i++;
+}
+// $data = explode("name", htmlspecialchars($line, ENT_QUOTES, 'utf-8'));
+$data = explode('class="name">', $line);
+// var_dump($data);
+$j = 0;
+$thread_pre = "";
+foreach ($data as $value) {
+    // echo $j; echo '<br>';
+    $del_sw = 0;    // 0: NG IDではない 1:NG IDである
+    if ($j > 0 && $j < 1001) {
+        $name = file("./del.txt");  //NG IDを読み込む
+        // var_dump($value); echo '<br>'; echo '<br>';
+        // echo '<br>';
+        $id = mb_strstr($value, "uid", false);
+        $id = mb_strstr($id, "span", true);
+        $id = mb_substr($id, 8, 9);
+        $thread = mb_strstr($value, "escaped", false);
+        $thread = mb_substr($thread, 9);
+        // echo $j.$id; echo '<br>';
+        // echo $thread; echo '<br><br>';
+        $thread = mb_strstr($thread, "/span", true);
+        $len = mb_strlen($thread);
+        $thread = mb_substr($thread, 0, $len - 1);
+
+        
+        if ($thread !== $thread_pre) {  //ひとつ前のスレとダブりがなければ、
+            foreach ($name as $row) {   //このIDがNGかどうか確認する。まずNG IDを配列にする
+                $row = trim($row);
+                if ($id === $row) {     //NG IDと一致するか
+                    $del_sw = 1;
+                }
+            }
+
+            if ($del_sw !== 1) {     //NG IDと一致しない場合
+                $pos = strpos($thread, "../test/read.cgi/soccer/"); //引用リンク(>6など)が、スレ内の何文字目に存在するかを$posに代入
+                if ($pos !== false) {   //引用リンクが存在する場合、
+                $pick = substr($thread, $pos, 35);  //リンク先のフォルダを修正  $thread内の$pos文字目の35文字を$pickとする
+                $thread = str_replace( $pick, "sanf_read.php#", $thread);   // $thread内の$pickをsanf_read.php#に置き換える
+                }
+                echo '<a id="' . $j . '">' . $j . '</a>';
+                echo '<form action="sanf_read.php#" method="get">';
+                echo '<input type="text" name="name" value="' . $id . '" style="border:none;">';
+                echo '<input type="hidden" name="num" value="' . $j . '">';
+                echo '<button type="submit" name="button" value="del" style="background-color:white; border:solid gray 1px;border-radius:50%;">削除</button>';
+                echo '<button type="submit" name="button" value="over" style="background-color:white; border:solid gray 1px;border-radius:50%;">読了</button>';
+                echo '</form>';
+
+                echo '<p style=" font-family:メイリオ; font-size: 20px; background: azure";>' . $thread . '</p>';
+                echo "\n";
+                $thread_pre = $thread;
+            }
+        }
+    }
+
+    $j++;
+}
+
+fclose($file);
+// require "sanf_select.php";
---
+++ sanf_select.php
@@ -0,0 +1,74 @@
+<!-- 国内サッカーのタイトル一覧から、サンフレッチェ広島を含むタイトルを表示する。
+そのタイトルの中で、読みたいスレッドを選択する。 -->
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sanfrecce</title>
+</head>
+
+<body>
+    <?php
+//     $top = "https://kizuna.5ch.net/test/read.cgi/soccer/"; //    左のurlにスレ番号,ファイル名を追加すると、スレッドが表示される。
+//     $title = filter_input(INPUT_POST, "list");
+//     if ($title !== null){        // ""をhtmlspecialcharsに使用することは、非推奨のため(deprecated)
+//     $title = htmlspecialchars($title, ENT_QUOTES, 'UTF-8');
+//     // var_dump ($title);
+//     // echo "<br>";
+//     }
+//     if ($title !== null) {
+//         $num = mb_substr($title, 17, 11);   //サンフレッチェ広島の文字がある行から、スレ番号のみ抽出
+//         $start = mb_strstr($title, " (", true);   // (　より前の部分の文字列を抜き出す
+//         // $name = mb_substr($start, mb_strrpos($start, ": ") + 1, mb_strlen($start));  // : より後の部分の文字列を抜き出し、ファイル名(拡張子なし)　↑↑↑サンフレッチェ広島Part2014↑↑↑
+//         // $name = trim($name);    //スペース部分を消す
+//         $name = "index";
+//         $title = $top . $num . $name . ".html";   //スレッドのurl
+//         $title = trim($title);
+//         $title = htmlspecialchars($title, ENT_QUOTES, 'UTF-8');
+//     // $title="https://weathernews.jp/";
+//     echo '<a href="'.$title.'">リンク</a>';
+//         echo "<br>";
+// // $ch = curl_init();
+// // curl_setopt($ch, CURLOPT_URL, $title);
+// // curl_setopt($ch, CURLOPT_HEADER, false);
+// // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// // curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+// // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+// // curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+// // $file = curl_exec($ch);
+
+// // var_dump($file);
+// // curl_close($ch);
+//     }
+
+    $top = "https://kizuna.5ch.net/test/read.cgi/soccer/"; //    左のurlにスレ番号,ファイル名を追加すると、スレッドが表示される。
+
+    $file = fopen("https://kizuna.5ch.net/soccer/subback.html", "r");
+    while (feof($file) === false) { //!feof($file)と同じ（falseの間続けますよ）の意味
+        $line = trim(fgets($file));
+        $line = mb_convert_encoding($line, "utf-8", "sjis"); // シフトJISからUTF-8に変換
+        $line = htmlspecialchars($line, ENT_QUOTES, 'UTF-8');
+        if (strstr($line, "サンフレッチェ広島")) {  //サンフレッチェ広島という文字がある場合、
+
+        $num = mb_substr($line, 17, 11);   //サンフレッチェ広島の文字がある行から、スレ番号のみ抽出
+// var_dump($top.$num);  echo "<br>";
+            // echo '<form method ="post">';
+            echo '<a href="'.$top.$num.'" style="width:900px; size=80; font-family:メイリオ; font-size: 20px;"> ' . $line . '</p>';
+
+            // echo '<input type="text" name="list" style="width:900px; size=80; font-family:メイリオ; font-size: 20px;" 
+            // value="' . $line . '" >';
+            // echo '<button type="submit" style="width:100px; background:#CCF; border:solid gray 0.5px; padding:10px;" 
+            // onMouseOut="this.style.background=\'#CCF\';" onMouseOver="this.style.background=\'#EEF\';">select</button>';
+            // echo '</form>';
+        }
+    }
+    fclose($file);
+
+
+    ?>
+</body>
+
+</html>
