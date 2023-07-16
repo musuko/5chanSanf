a450556a650578734d9701d6813c52a94fcf1fc9
musuko <100979661+musuko@users.noreply.github.com>
2023-07-16 12:13:35 +0900

    Add files via upload
    重複スレの確認位置を、前方へ移動し、des_swの条件に含めることとした。従来、スレを削除したとき、リンクにジャンプできないことがときどきあったことを修正。

--- index.php
+++ index.php
@@ -27,36 +27,36 @@ echo "<br> \n";
 // htmlを読み込む
 $file = fopen("./sanf.html", "r");
 $i = 1;
-$total_lines= 1452;    //$total_linesは、htmlの中で、threadが書き込まれている行。
-while ($i <= $total_lines) { //!feof($file)と同じ（falseの間続けますよ）の意味
+$thread_line_num= 1452;    //$thread_line_numは、htmlの中で、threadが書き込まれている行。
+while ($i <= $thread_line_num) { //!feof($file)と同じ（falseの間続けますよ）の意味
     // while (feof($file) === false) { //!feof($file)と同じ（falseの間続けますよ）の意味
     $line = trim(fgets($file));
     $line = mb_convert_encoding($line, "utf-8", "sjis"); // シフトJISからUTF-8に変換
     //thread行の不要部分を取り除く
-    if ($i === $total_lines) {
-        $line = mb_strstr($line, '});</script>', false);   // 指定文字より後の部分の文字列を抜き出す
-        $line = mb_substr($line, 12);       //指定文字数を先頭から取り除く
-        $line = mb_strstr($line, '<div class="navmenu">', true);   // 指定文字より前の部分の文字列を抜き出す
+    if ($i === $thread_line_num) {
+        $thread_line = mb_strstr($line, '});</script>', false);   // 指定文字より後の部分の文字列を抜き出す
+        $thread_line = mb_substr($thread_line, 12);       //指定文字数を先頭から取り除く
+        $thread_line = mb_strstr($thread_line, '<div class="navmenu">', true);   // 指定文字より前の部分の文字列を抜き出す
+        $thread_line = trim($thread_line);
         //trueを指定した場合、指定した文字列より前の文字列を取得します。指定しない場合（false）、指定した文字列以降の文字列を取得します。
     }
     $i++;
 }
-// echo $i."piyo".($line)."pao";echo '<br>';
 fclose($file);
 
 
 //thread行の前後不要部分を取り除く
-// $data = explode("name", htmlspecialchars($line, ENT_QUOTES, 'utf-8'));
 
-$data = explode('</section></article>', $line);    //threadが書き込まれているテキストを、各threadの配列にする。$data[0]は0002スレの情報
-
+// echo htmlspecialchars($thread_line);
+$data = explode('</section></article>', $thread_line);    //threadが書き込まれているテキストを、各threadの配列にする。$data[0]は0002スレの情報
+// var_dump($data);
 // 配列に、thread, id, 表示可否情報を収める
 $j = 2;
 $num_jump = 1;  //ボタンを押した後のジャンプ先、初期値設定
 $thread[1] = "";
 $id[1] = "";
-foreach ($data as $value) {
-    // echo $j. '<br>'; echo $value. '<br>';
+foreach ($data as $value) { //$data[2] thread idが2
+    // echo $j. '<br>'; echo htmlspecialchars($value). '<br>';
 
     $del_sw[$j] = 0;    // 0: NG IDではない 1:NG IDである
     if ($j < 1001) {
@@ -73,25 +73,28 @@ foreach ($data as $value) {
         
         // echo $j, $id[$j], $thread[$j]; echo '<br>';
 
-//threadをNG idを除き表示する
+ //threadをNG id,重複threadを除き表示する
         $ng_name = file("./del.txt");  //NG IDを読み込む
         foreach ($ng_name as $row) {   //このIDがNGかどうか確認する。まずNG IDを配列にする
             $row = trim($row);
-            if ($id[$j] === $row) {     //NG IDと一致するか
+            if ($id[$j] === $row) {     //NG IDと一致するか、または重複threadの場合
                 $del_sw[$j] = 1;        //NG IDと一致した場合、$del_swを1にし、表示不可とする。
             }
+        }
+        if ($thread[$j] === $thread[$j-1])  {     //重複threadの場合
+            $del_sw[$j] = 1;        //$del_swを1にし、表示不可とする。
         }
         if ($del_sw[$j] !== 1 && $j <= $num) {    //表示可能かつ削除ボタンを押したthread番号以下の場合
             $num_jump = $j;   //ジャンプ先のthread番号
         }
-        $jmax=$j;    //読み込みthread数
+        $jmax=$j-1;    //読み込みthread数。$dataのexplodeで一行余計になるので、減らす。
     }
     $j++;
 }
 
 
 
-//$numが1より大きい場合、ジャンプする先をリンクで表示する
+//ジャンプする先をリンクで表示する。ただし、$numが1より大きい場合。
 if ($num > 1) {
     echo '<a href="#' . ($num_jump) . '">ID = #' . ($num_jump) . 'へジャンプ</a>';
     echo "<br> \n";
@@ -101,15 +104,15 @@ if ($num > 1) {
 //NGと重複を除いたthreadを表示する
 // $thread[0] = "";
 for ($j=2; $j<=$jmax; ++$j){
-
-            if ($del_sw[$j] !== 1) {     //NG IDと一致しない場合
-                if ($thread[$j] !== $thread[$j-1]) {    //重複threadではない場合
-                    // echo $j, $thread[$j];
+    
+    if ($del_sw[$j] !== 1) {     //NG IDと一致せず、重複スレではない場合
+        // echo $j." ".$del_sw[$j]." ".$id[$j]." ".$thread[$j];echo "<br>";
                     $pos = strpos($thread[$j], "../test/read.cgi/soccer/"); //引用リンク(>6など)が、スレ内の何文字目に存在するかを$posに代入
                     if ($pos !== false) {   //引用リンクが存在する場合、
                         $pick = substr($thread[$j], $pos, 35);  //リンク先のフォルダを修正  $thread[$j]内の$pos文字目の35文字を$pickとする
                         $thread[$j] = str_replace($pick, "index.php#", $thread[$j]);   // $thread[$j]内の$pickをindex.php#に置き換える
                     }
+
 //実際に表示するthread                
                 // echo  $j;
                 echo '<a id="' . $j . '">' . $j . '</a>';   //誤記に見えて意味がある。この番号のスレにジャンプするために使用。
@@ -122,7 +125,6 @@ for ($j=2; $j<=$jmax; ++$j){
 
                 echo '<p style=" font-family:メイリオ; font-size: 20px; background: azure";>' . $thread[$j] . '</p>';
                 echo "\n";
-                }
             }
         
     
