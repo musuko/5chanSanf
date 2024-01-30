53ca2760a1aa9f5ca4278a504b7c852345ea144a
musuko <100979661+musuko@users.noreply.github.com>
2024-01-30 20:47:17 +0900

    ファイル保存、再読み込みの手間を省略。
    リンク先のhtmlを変数に取り込み、これを表示させる方式に変更。

--- sanf_select.php
+++ sanf_select.php
@@ -1,79 +1,34 @@
 <!-- 国内サッカーのタイトル一覧から、サンフレッチェ広島を含むタイトルをリンクで表示する。 -->
 
-<!DOCTYPE html>
-<html lang="ja">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sanfrecce</title>
-</head>
-
-<body>
     <?php
-//     $top = "https://kizuna.5ch.net/test/read.cgi/soccer/"; //    左のurlにスレ番号,ファイル名を追加すると、スレッドが表示される。
-//     $title = filter_input(INPUT_POST, "list");
-//     if ($title !== null){        // ""をhtmlspecialcharsに使用することは、非推奨のため(deprecated)
-//     $title = htmlspecialchars($title, ENT_QUOTES, 'UTF-8');
-//     // var_dump ($title);
-//     // echo "<br>";
-//     }
-//     if ($title !== null) {
-//         $num = mb_substr($title, 17, 11);   //サンフレッチェ広島の文字がある行から、スレ番号のみ抽出
-//         $start = mb_strstr($title, " (", true);   // (　より前の部分の文字列を抜き出す
-//         // $name = mb_substr($start, mb_strrpos($start, ": ") + 1, mb_strlen($start));  // : より後の部分の文字列を抜き出し、ファイル名(拡張子なし)　↑↑↑サンフレッチェ広島Part2014↑↑↑
-//         // $name = trim($name);    //スペース部分を消す
-//         $name = "index";
-//         $title = $top . $num . $name . ".html";   //スレッドのurl
-//         $title = trim($title);
-//         $title = htmlspecialchars($title, ENT_QUOTES, 'UTF-8');
-//     // $title="https://weathernews.jp/";
-//     echo '<a href="'.$title.'">リンク</a>';
-//         echo "<br>";
-// // $ch = curl_init();
-// // curl_setopt($ch, CURLOPT_URL, $title);
-// // curl_setopt($ch, CURLOPT_HEADER, false);
-// // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// // curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-// // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-// // curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-// // $file = curl_exec($ch);
-
-// // var_dump($file);
-// // curl_close($ch);
-//     }
 
     $top = "https://kizuna.5ch.net/test/read.cgi/soccer/"; //    左のurlにタイトル番号を追加すると、スレッドが表示される。
 
     $file = fopen("https://kizuna.5ch.net/soccer/subback.html", "r");
+    $i = 0;
     while (feof($file) === false) { //!feof($file)と同じ（falseの間続けますよ）の意味
-        $line = trim(fgets($file));
-        $line = mb_convert_encoding($line, "utf-8", "sjis"); // シフトJISからUTF-8に変換
-        if (strstr($line, "サンフレッチェ広島")) {  //サンフレッチェ広島という文字がある場合、
-            $title = mb_substr($line, 25);       //指定文字数を先頭から取り除く
-            $title = mb_strstr($title, '</a>', true);   // 指定文字より前の部分の文字列を抜き出す
-            // echo ($title);  echo "<br>";
-            $line = htmlspecialchars($line, ENT_QUOTES, 'UTF-8');   // タイトル番号を抽出するため、html文字を分解する
-            // echo ($line);  echo "<br>";
-            $num = mb_substr($line, 17, 11);   //サンフレッチェ広島の文字があるhtml文字列から、タイトル番号のみ抽出
-// var_dump($top.$num);  echo "<br>";
-            // echo '<form method ="post">';
-            echo '<a href="'.$top.$num.'" style="width:900px; size=80; font-family:メイリオ; font-size: 20px;"> ' . $title . '</a>';
-            echo "<br>";
-            // echo '<p><a href="'.$top.$num.'" style="width:900px; size=80; font-family:メイリオ; font-size: 20px;"> ' . $line . '</a></p>';
+        $line[$i] = trim(fgets($file));
+        $line[$i] = mb_convert_encoding($line[$i], "utf-8", "sjis"); // シフトJISからUTF-8に変換
+        if (strstr($line[$i], "サンフレッチェ広島")) {  //サンフレッチェ広島という文字がある場合、
+            $title[$i] = mb_substr($line[$i], 25);       //指定文字数を先頭から取り除く
+            $title[$i] = mb_strstr($title[$i], '</a>', true);   // 指定文字より前の部分の文字列を抜き出す
+            // echo ($title[$i]);  echo "<br>";
+            $line[$i] = htmlspecialchars($line[$i], ENT_QUOTES, 'UTF-8');   // タイトル番号を抽出するため、html文字を分解する
+            // echo ($line[$i]);  echo "<br>";
+            $num[$i] = mb_substr($line[$i], 17, 10);   //サンフレッチェ広島の文字があるhtml文字列から、タイトル番号のみ抽出。特殊文字に注意 < と　"
+            // echo ($top.'---'.$num[$i]);  echo "<br>";
 
-            // echo '<input type="text" name="list" style="width:900px; size=80; font-family:メイリオ; font-size: 20px;" 
-            // value="' . $line . '" >';
-            // echo '<button type="submit" style="width:100px; background:#CCF; border:solid gray 0.5px; padding:10px;" 
-            // onMouseOut="this.style.background=\'#CCF\';" onMouseOver="this.style.background=\'#EEF\';">select</button>';
-            // echo '</form>';
+            // リンク先のHTMLを保存するためのコード
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $top . $num[$i]);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $html[$i] = curl_exec($ch);
+            curl_close($ch);
+            // file_put_contents($num[$i] . '.html', $html[$i]);
+            echo '<a href="index.php?id=' . $i . '" style="width:900px; size=80; font-family:メイリオ; font-size: 20px;"> ' . $title[$i] . '</a>';
+            echo "<br>";
+            $i++;
         }
     }
     fclose($file);
-
-
     ?>
-</body>
-
-</html>
